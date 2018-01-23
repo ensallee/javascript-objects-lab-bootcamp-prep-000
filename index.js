041947258573267e27c9f1ec28bb81b171c7c7d6
Betsy Sallee <ensallee@gmail.com>
2018-01-23 01:03:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {prop: 1, prop2: 2});
+  return Object.assign({}, object, {key: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
